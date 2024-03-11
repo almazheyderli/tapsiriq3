72be2321f271b5a9345a3fbb9afe26091f4c8d64
almazheyderli <bd7g7vr18@code.edu.az>
2024-03-12 03:05:41 +0400

    array

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 45, 67, 12 };
+            int min = arr[0];
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine(min);
+
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { -2, 3, 4, -5 };
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                if (a[i] < 0)
+                {
+                    a[i] = a[i] * -1;
+                }
+                Console.WriteLine(a[i]);    
+
+            }
+        }
+    }
+}
